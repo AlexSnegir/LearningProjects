1b687867ab75bf25f62c2545b0df9c67021e3489
Aleksandre Kapanadze <aleksandre.kapanadze.89@gmail.com>
2024-01-24 14:47:30 +0400

    Pluralsight. DDD in Practice. Part 2.

---
+++ Pluralsight/Vladimir Khorikov/DddInPractice/DddInPractice.Logic/SnackMachine.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System;
+using static DddInPractice.Logic.Money;
+
+namespace DddInPractice.Logic
+{
+    public sealed class SnackMachine : Entity
+    {
+        public Money MoneyInside { get; private set; } = None;
+        public Money MoneyInTransaction { get; private set; } = None;
+
+        public void InsertMoney(Money money)
+        {
+            Money[] coinsAndNotes =
+            {
+                Cent, TenCent, Quarter, Dollar, FiveDollar, TwentyDollar
+            };
+
+            if (!coinsAndNotes.Contains(money))
+                throw new InvalidOperationException();
+
+            MoneyInTransaction += money;
+        }
+
+        public void ReturnMoney()
+        {
+            MoneyInTransaction = None;
+        }
+
+        public void BuySnack()
+        {
+            MoneyInside += MoneyInTransaction;
+
+            MoneyInTransaction = None;
+        }
+    }
+}
